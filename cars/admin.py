1af2876c779b483c086c121afd8460a5b4375da1
Kauan Zembruski <kauanzem@icloud.com>
2026-03-03 21:42:36 -0300

    primeiro commit do projeto

---
+++ cars/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from cars.models import Car,Brand
+# Register your models here.
+
+class Brand_Admin(admin.ModelAdmin):
+    lsit_display = ('name',)
+    search_fields = ('name',)
+    
+    
+class Car_Admin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year' ,  'model_year',  'value' )
+    search_fields = ('model','brand')
+
+
+    
+
+admin.site.register(Brand,Brand_Admin)
+admin.site.register(Car,Car_Admin)
---
+++ cars/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from cars.models import Car
+
+    
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
+    
+    def clean_value(self):#validação no form do django
+        value = self.cleaned_data.get('value')
+        if value < 15000:
+            self.add_error('value','Valor minimo do carro deve ser de R$15.000')
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year','Não é possivel add carros fabricados antes de 1975')
+        return factory_year
